bcb9e7790c3b56c2cdb05b2b98f5b4a7c4421b41
Undergamer64 <150269652+Undergamer64@users.noreply.github.com>
2026-06-27 14:59:08 +0200

    changed version

--- Assets/Scripts/CardsManager.cs
+++ Assets/Scripts/CardsManager.cs
@@ -129,7 +129,7 @@ public class CardsManager : MonoBehaviour
     {
         if (_weaponsDisplayedText)
         {
-            _weaponsDisplayedText.text = _weaponsEnabled.ToString() + '/' + _maxWeaponsEnabled.ToString() + " Cardes actives";
+            _weaponsDisplayedText.text = _weaponsEnabled.ToString() + '/' + _maxWeaponsEnabled.ToString() + " Cartes actives";
         }
 
         _cards[cardIndex].image.color = !_cardsActive[cardIndex] ? Color.gray : Color.white;
